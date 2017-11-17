c0928e4027eb9910e477d422abaad78cfb78c422
kwhisler <kwhisler@uncc.edu>
2017-11-16 22:08:49 -0500

    added sanding activity

---
+++ app/src/main/java/com/example/daniel/a4155cmp/SandingActivity.java
@@ -0,0 +1,13 @@
+package com.example.daniel.a4155cmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sanding);
+    }
+}
