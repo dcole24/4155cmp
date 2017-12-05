2a3f97e77faca5bcc63f670c13061deff1e2d5fd
Tommy Huynh <Tommy@wifi_stu-10-38-43-84.uncc.edu>
2017-12-05 16:26:54 -0500

    added all files

--- app/src/main/java/com/example/daniel/a4155cmp/PaintingActivity.java
+++ app/src/main/java/com/example/daniel/a4155cmp/PaintingActivity.java
@@ -1,12 +1,15 @@
 package com.example.daniel.a4155cmp;
 
 
-
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.VideoView;
 
 
 public class PaintingActivity extends AppCompatActivity {
@@ -16,12 +19,22 @@ public class PaintingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting);
 
+        Button videoBtn = findViewById(R.id.videoButton);
+
         final CheckBox checkBox = findViewById(R.id.CheckBox_Painting);
         if (checkBox.isChecked()) {
             checkBox.setChecked(false);
         }
 
 
+
+        videoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                vView.start();
+            }
+        });
 
 
     }
