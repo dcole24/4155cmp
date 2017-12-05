57b610092741365c5dbe3237a5f62beb17f594a3
Tommy Huynh <Tommy@wifi_stu-10-38-43-84.uncc.edu>
2017-12-05 16:02:29 -0500

    updated the save instance on checkbox

--- app/src/main/java/com/example/daniel/a4155cmp/PaintingActivity.java
+++ app/src/main/java/com/example/daniel/a4155cmp/PaintingActivity.java
@@ -1,11 +1,12 @@
 package com.example.daniel.a4155cmp;
 
 
-import android.content.Intent;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 
 
 public class PaintingActivity extends AppCompatActivity {
@@ -15,11 +16,39 @@ public class PaintingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painting);
 
+        final CheckBox checkBox = findViewById(R.id.CheckBox_Painting);
+        if (checkBox.isChecked()) {
+            checkBox.setChecked(false);
+        }
+
+
+
 
     }
 
     @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+    public void onPause() {
+        super.onPause();
+        final CheckBox checkBox = findViewById(R.id.CheckBox_Painting);
+        save(checkBox.isChecked());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        final CheckBox checkBox = findViewById(R.id.CheckBox_Painting);
+        checkBox.setChecked(load());
+    }
+
+    private void save(final boolean isChecked) {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("check", isChecked);
+        editor.commit();
+    }
+
+    private boolean load() {
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("check", false);
     }
 }
